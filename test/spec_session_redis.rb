b0bf9708584fe896410fb39df308d54ef61ccc0a
Harry Vangberg <harry@vangberg.name>
2010-06-22 01:49:37 +0200

    s/pool/redis/

--- test/spec_session_redis.rb
+++ test/spec_session_redis.rb
@@ -34,15 +34,15 @@ describe Rack::Session::Redis do
   end
 
   it "creates a new cookie" do
-    pool = Rack::Session::Redis.new(incrementor)
-    res = Rack::MockRequest.new(pool).get("/")
+    redis = Rack::Session::Redis.new(incrementor)
+    res = Rack::MockRequest.new(redis).get("/")
     res["Set-Cookie"].should.match session_match
     res.body.should.equal '{"counter"=>1}'
   end
 
   it "determines session from a cookie" do
-    pool = Rack::Session::Redis.new(incrementor)
-    req = Rack::MockRequest.new(pool)
+    redis = Rack::Session::Redis.new(incrementor)
+    req = Rack::MockRequest.new(redis)
     cookie = req.get("/")["Set-Cookie"]
     req.get("/", "HTTP_COOKIE" => cookie).
       body.should.equal '{"counter"=>2}'
@@ -51,93 +51,93 @@ describe Rack::Session::Redis do
   end
 
   it "survives nonexistant cookies" do
-    pool = Rack::Session::Redis.new(incrementor)
-    res = Rack::MockRequest.new(pool).
+    redis = Rack::Session::Redis.new(incrementor)
+    res = Rack::MockRequest.new(redis).
       get("/", "HTTP_COOKIE" => "#{session_key}=blarghfasel")
     res.body.should.equal '{"counter"=>1}'
   end
 
   it "deletes cookies with :drop option" do
-    pool = Rack::Session::Redis.new(incrementor)
-    req = Rack::MockRequest.new(pool)
-    drop = Rack::Utils::Context.new(pool, drop_session)
+    redis = Rack::Session::Redis.new(incrementor)
+    req = Rack::MockRequest.new(redis)
+    drop = Rack::Utils::Context.new(redis, drop_session)
     dreq = Rack::MockRequest.new(drop)
 
     res0 = req.get("/")
     session = (cookie = res0["Set-Cookie"])[session_match]
     res0.body.should.equal '{"counter"=>1}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res1 = req.get("/", "HTTP_COOKIE" => cookie)
     res1["Set-Cookie"][session_match].should.equal session
     res1.body.should.equal '{"counter"=>2}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res2 = dreq.get("/", "HTTP_COOKIE" => cookie)
     res2["Set-Cookie"].should.equal nil
     res2.body.should.equal '{"counter"=>3}'
-    pool.redis.dbsize.should.equal 0
+    redis.redis.dbsize.should.equal 0
 
     res3 = req.get("/", "HTTP_COOKIE" => cookie)
     res3["Set-Cookie"][session_match].should.not.equal session
     res3.body.should.equal '{"counter"=>1}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
   end
 
   it "provides new session id with :renew option" do
-    pool = Rack::Session::Redis.new(incrementor)
-    req = Rack::MockRequest.new(pool)
-    renew = Rack::Utils::Context.new(pool, renew_session)
+    redis = Rack::Session::Redis.new(incrementor)
+    req = Rack::MockRequest.new(redis)
+    renew = Rack::Utils::Context.new(redis, renew_session)
     rreq = Rack::MockRequest.new(renew)
 
     res0 = req.get("/")
     session = (cookie = res0["Set-Cookie"])[session_match]
     res0.body.should.equal '{"counter"=>1}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res1 = req.get("/", "HTTP_COOKIE" => cookie)
     res1["Set-Cookie"][session_match].should.equal session
     res1.body.should.equal '{"counter"=>2}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res2 = rreq.get("/", "HTTP_COOKIE" => cookie)
     new_cookie = res2["Set-Cookie"]
     new_session = new_cookie[session_match]
     new_session.should.not.equal session
     res2.body.should.equal '{"counter"=>3}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res3 = req.get("/", "HTTP_COOKIE" => new_cookie)
     res3["Set-Cookie"][session_match].should.equal new_session
     res3.body.should.equal '{"counter"=>4}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
   end
 
   it "omits cookie with :defer option" do
-    pool = Rack::Session::Redis.new(incrementor)
-    req = Rack::MockRequest.new(pool)
-    defer = Rack::Utils::Context.new(pool, defer_session)
+    redis = Rack::Session::Redis.new(incrementor)
+    req = Rack::MockRequest.new(redis)
+    defer = Rack::Utils::Context.new(redis, defer_session)
     dreq = Rack::MockRequest.new(defer)
 
     res0 = req.get("/")
     session = (cookie = res0["Set-Cookie"])[session_match]
     res0.body.should.equal '{"counter"=>1}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res1 = req.get("/", "HTTP_COOKIE" => cookie)
     res1["Set-Cookie"][session_match].should.equal session
     res1.body.should.equal '{"counter"=>2}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res2 = dreq.get("/", "HTTP_COOKIE" => cookie)
     res2["Set-Cookie"].should.equal nil
     res2.body.should.equal '{"counter"=>3}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
 
     res3 = req.get("/", "HTTP_COOKIE" => cookie)
     res3["Set-Cookie"][session_match].should.equal session
     res3.body.should.equal '{"counter"=>4}'
-    pool.redis.dbsize.should.equal 1
+    redis.redis.dbsize.should.equal 1
   end
 
   # anyone know how to do this better?
@@ -148,13 +148,13 @@ describe Rack::Session::Redis do
     end
 
     warn 'Running multithread tests for Session::Redis'
-    pool = Rack::Session::Redis.new(incrementor)
-    req = Rack::MockRequest.new(pool)
+    redis = Rack::Session::Redis.new(incrementor)
+    req = Rack::MockRequest.new(redis)
 
     res = req.get('/')
     res.body.should.equal '{"counter"=>1}'
     cookie = res["Set-Cookie"]
-    sess_id = cookie[/#{pool.key}=([^,;]+)/,1]
+    sess_id = cookie[/#{redis.key}=([^,;]+)/,1]
 
     delta_incrementor = lambda do |env|
       # emulate disconjoinment of threading
@@ -163,7 +163,7 @@ describe Rack::Session::Redis do
       env['rack.session'][(Time.now.usec*rand).to_i] = true
       incrementor.call(env)
     end
-    tses = Rack::Utils::Context.new pool, delta_incrementor
+    tses = Rack::Utils::Context.new redis, delta_incrementor
     treq = Rack::MockRequest.new(tses)
     tnum = rand(7).to_i+5
     r = Array.new(tnum) do
@@ -176,7 +176,7 @@ describe Rack::Session::Redis do
       res.body.should.include '"counter"=>2'
     end
 
-    session = pool.pool[sess_id]
+    session = redis.redis[sess_id]
     session.size.should.equal tnum+1 # counter
     session['counter'].should.equal 2 # meeeh
   end
